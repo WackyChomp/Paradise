8e5e82de5f98b8e022ff9f65e3a2d9954e723bc6
Wilson Chan <wilsonchan54@gmail.com>
2020-11-13 23:51:42 -0500

    Remove styling in App.css / adding margin in index.css / use BEM naming convention

--- paradise/src/App.js
+++ paradise/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    //Following BEM convention
+    <div className="app">
       <h1>Change da world my final message, Goodbye :O</h1>
     </div>
   );
