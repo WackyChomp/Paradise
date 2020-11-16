29b1f053d581d277a1f22c97b904627d782ec044
Wilson Chan <wilsonchan54@gmail.com>
2020-11-15 20:26:36 -0500

    Creating product__info/__price/__rating / adding sample image

--- paradise/src/Home.js
+++ paradise/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
     return (
@@ -9,18 +10,18 @@ function Home() {
                 src = "https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428584220_.jpg" 
                 alt = ""/>
 
-                <div className = "home_row">
+                <div className = "home__row">
+                    <Product />
+                    {/*Product*/}
+                </div>
+
+                <div className = "home__row">
+                    {/*Product*/}
                     {/*Product*/}
                     {/*Product*/}
                 </div>
 
-                <div className = "home_row">
-                    {/*Product*/}
-                    {/*Product*/}
-                    {/*Product*/}
-                </div>
-
-                <div className = "home_row">
+                <div className = "home__row">
                     {/*Product*/}
                 </div>
 
--- paradise/src/Product.js
+++ paradise/src/Product.js
@@ -5,8 +5,23 @@ function Product() {
     return (
         <div className = "product">
             
+            <div className = "product__info">
+                <p>The Wack Inflatable Tube Man</p>
+                <p className = "product__price">
+                    <small>$</small>
+                    <strong>4.20</strong>
+                </p>
+                
+                <div className = "product__rating">
+                    <p>🌟</p>
+                </div>
+            </div>
+
+            <img src = "https://images-na.ssl-images-amazon.com/images/I/714ixvtUpZL._AC_SL1500_.jpg" alt = ""/>
+
+            <button>Add to Basket</button>
         </div>
-    )
+    );
 }
 
-export default Product
+export default Product;
