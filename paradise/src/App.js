80988587256c0212893b0eabc45e05537c553234
Wilson Chan <wilsonchan54@gmail.com>
2020-11-15 23:02:33 -0500

    npm installed react-router-dom

--- paradise/src/App.js
+++ paradise/src/App.js
@@ -2,16 +2,29 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom"
 
 function App() {
   return (
     //Following BEM convention
+    <Router>
     <div className="app">
       <h1>Change da world my final message, Goodbye :O</h1>
-      <Header />
-      <Home />
+      <Switch>
+        <Route path = "/checkout">
+          <header />
 
+        </Route>
+
+        <Route path = "/">
+          <Header />
+          <Home />
+        </Route>
+      </Switch>
     </div>
+    </Router>
+
   );
 }
 
