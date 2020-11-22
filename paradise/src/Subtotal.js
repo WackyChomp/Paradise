fa5b238169fcd53a7d1055da5f9e9c58a601f74c
Wilson Chan <wilsonchan54@gmail.com>
2020-11-21 23:59:21 -0500

    Creating function Subtotal / Adding Subtotal import to Checkout.js

--- paradise/src/Subtotal.js
+++ paradise/src/Subtotal.js
@@ -5,7 +5,24 @@ import CurrencyFormat from "react-currency-format";
 function Subtotal() {
     return (
         <div classname = 'subtotal'>
-            
+            <CurrencyFormat
+                renderText = { (value) => (
+                    <>
+                        <p>
+                            Subtotal (0 items): <strong>0</strong>
+                        </p>
+                        <small className = "subtotal_gift">
+                            <input type = "checkbox" /> This order contains a gift
+                        </small>
+                    </>
+                )}
+
+                decimalScale = {2}
+                value = {getBasketTotal(basket)}
+                displayType = {"Text"}
+                thousandSeparator = {true}
+                prefix = {"E"}
+            />
         </div>
     )
 }
