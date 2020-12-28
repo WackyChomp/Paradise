4eab9e31868c70d808a0c50d88a3f1fa15d04a43
Wilson Chan <wilsonchan54@gmail.com>
2020-12-28 00:00:26 -0500

    Initial commit for Login Page / adding login route in App.js / converting the Sign In header into a clickable link for testing

--- paradise/src/App.js
+++ paradise/src/App.js
@@ -14,12 +14,13 @@ function App() {
       <Header />
 
       <Switch>
+        <Route path = "/login">
+            <h1>This is the Login Page</h1>
+        </Route>
         <Route path = "/checkout">
             <Checkout />
         </Route>
-
         <Route path = "/">
-          <Header />
           <Home />
         </Route>
       </Switch>
