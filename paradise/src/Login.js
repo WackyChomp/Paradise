07ba55b14fed2877a5d8ceb31c186b0b03b19aea
Wilson Chan <wilsonchan54@gmail.com>
2020-12-29 23:35:33 -0500

    Creating className login__container with form that takes user input in Login.js / styling and centering the logo in Login.css

--- paradise/src/Login.js
+++ paradise/src/Login.js
@@ -11,6 +11,18 @@ function Login() {
                 src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png'
             />
             </Link>
+
+            <div className = 'login__container'>
+                <h1>Sign In</h1>
+                
+                <form>
+                    <h5>E-mail</h5>
+                    <input type = 'text' />
+                    <h5>Password</h5>
+                    <input type = 'text' />
+                </form>
+                    
+            </div>
         
         </div>
     )
