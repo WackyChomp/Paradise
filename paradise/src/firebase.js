eeea007603f2c55e22a1ed379e06712f922c7e8c
Wilson Chan <wilsonchan54@gmail.com>
2021-01-02 23:21:19 -0500

    Adding firebase SDK code snippet to firebase.js / npm install firebase and global firebase-tools

--- paradise/src/firebase.js
+++ paradise/src/firebase.js
@@ -1,4 +1,12 @@
-/*
-Empty for now.
-Will add firebase code later on
-*/
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "paradise-2f204.firebaseapp.com",
+    projectId: "paradise-2f204",
+    storageBucket: "paradise-2f204.appspot.com",
+    messagingSenderId: "116687576479",
+    appId: "1:116687576479:web:80e8577b8b4ad5916231aa",
+    measurementId: "G-0ZQPCPLFC5"
+  };
