83f26495356842a2d2819c83ec558f6875539bcc
Wilson Chan <wilsonchan54@gmail.com>
2021-01-03 11:43:49 -0500

    Initializing firebaseApp with database and authentication / export ready for use outside of firebase.js

--- paradise/src/firebase.js
+++ paradise/src/firebase.js
@@ -10,3 +10,11 @@ const firebaseConfig = {
     appId: "1:116687576479:web:80e8577b8b4ad5916231aa",
     measurementId: "G-0ZQPCPLFC5"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);     /*pass in firebaseConfig from above to initialize*/
+
+const db = firebaseApp.firestore();        /*initialize the database with live firestore*/
+const awth = firebase.auth();
+
+export { db, awth };
+
